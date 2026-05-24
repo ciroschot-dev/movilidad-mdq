dc0d51164479f966065539592eefd55d32e9fefb
ciroschot-dev <cirschot@gmail.com>
2026-05-24 18:09:53 -0300

    borre metodo no usado

--- src/main/java/com/example/movilidadmdq/security/SecurityConfig.java
+++ src/main/java/com/example/movilidadmdq/security/SecurityConfig.java
@@ -46,13 +46,13 @@ public class SecurityConfig
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
     {
         return config.getAuthenticationManager();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
     {
         http
                 .csrf(AbstractHttpConfigurer::disable)
