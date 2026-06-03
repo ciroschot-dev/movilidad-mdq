a73c762ee50ec8bd91eb3c549610cc53df2d8b78
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 00:06:27 -0300

    swagger2

--- src/main/java/com/example/movilidadmdq/dto/RegistroRequest.java
+++ src/main/java/com/example/movilidadmdq/dto/RegistroRequest.java
@@ -4,7 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegistroRequest(
+        @Schema(description = "Nombre de usuario", example = "ciroschot23")
         @NotBlank String username,
+
+        @Schema(description = "Contraseña del usuario", example = "surfbody2333")
         @NotBlank String password,
+
+        @Schema(description ="Email del usuario", example = "ciroschot@gmail.com")
         @NotBlank @Email String email
 ) {}
