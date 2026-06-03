627a3be2e4ab1f637562f56481bd6253fd357d6d
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 00:06:27 -0300

    swagger2

--- src/main/java/com/example/movilidadmdq/dto/LoginRequest.java
+++ src/main/java/com/example/movilidadmdq/dto/LoginRequest.java
@@ -1,5 +1,11 @@
 package com.example.movilidadmdq.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequest(@NotBlank String username, @NotBlank String password) {}
+public record LoginRequest(
+        @Schema(description = "Nombre de usuario para ingresar", example = "Morehidalggo")
+        @NotBlank String username,
+
+        @Schema(description = "Contraseña para ingresar", example = "Ramona00.2")
+        @NotBlank String password) {}
