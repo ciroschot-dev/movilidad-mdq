b464e9e23bbc045a2a5ef9c97aab35c2e8ebf033
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 13:28:39 -0300

    swagger3

--- src/main/java/com/example/movilidadmdq/dto/ActualizarUsuarioRequest.java
+++ src/main/java/com/example/movilidadmdq/dto/ActualizarUsuarioRequest.java
@@ -4,7 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record ActualizarUsuarioRequest(
+        @Schema(description = "Nombre de usuario", example = "morehidalggo")
         @NotBlank String username,
+
+        @Schema(description = "Email del usuario", example = "morehidalggo@gmail.com")
         @NotBlank @Email String email,
+
+        @Schema(description = "Contraseña del usuario", example = "morena123")
         String password
 ) {}
--- src/main/java/com/example/movilidadmdq/dto/ViajeFrecuenteResponse.java
+++ src/main/java/com/example/movilidadmdq/dto/ViajeFrecuenteResponse.java
@@ -1,8 +1,14 @@
 package com.example.movilidadmdq.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ViajeFrecuenteResponse(
+        @Schema(description = "Origen del viaje frecuente", example = "Colon 1736")
         String origen,
+
+        @Schema(description = "Destino del viaje frecuente", example = "Saavedra 56")
         String destino,
+
+        @Schema(description = "Cantidad de veces que se pidio el vijae", example = "3")
         Long cantidad
 ) {
 }
