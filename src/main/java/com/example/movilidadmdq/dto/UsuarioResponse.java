5a54ad380761c92b104434f85d6836bfc1743b1d
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 13:28:39 -0300

    swagger3

--- src/main/java/com/example/movilidadmdq/dto/UsuarioResponse.java
+++ src/main/java/com/example/movilidadmdq/dto/UsuarioResponse.java
@@ -2,4 +2,16 @@ package com.example.movilidadmdq.dto;
 
 import com.example.movilidadmdq.enums.Role;
 
-public record UsuarioResponse(Long id, String username, String email, Role role) {}
+public record UsuarioResponse(
+        @Schema(description="Id del usuario", example = "33")
+        Long id,
+
+        @Schema(description = "nombre del usuario", example=" pepito123")
+        String username,
+
+        @Schema(description = "email del usuario", example = "pepito@gmail.com")
+        String email,
+
+        @Schema(description ="rol del usuario", example = "admin")
+        Role role
+) {}
--- src/main/java/com/example/movilidadmdq/dto/ViajeHistorialResponse.java
+++ src/main/java/com/example/movilidadmdq/dto/ViajeHistorialResponse.java
@@ -4,13 +4,30 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ViajeHistorialResponse(
+        @Schema(description = "Id del usuario", example = "33")
         Long id,
+
+        @Schema(description = "Origen del viaje",example = "colon 1736")
         String origen,
+
+        @Schema(description = "Destino del viaje",example = "Tuyu 2024")
         String destino,
+
+        @Schema(description = "Distancia en metros entre origen y destino", example ="5km")
         Long distanciaEnMetros,
+
+        @Schema(description = "Tiempo estimado en llegar desde origen a destino", example = "10")
         Integer tiempoEstimadoMin,
+
+        @Schema(description = "Precio del taxi del viaje", example = "5000")
         BigDecimal precioTaxi,
+
+        @Schema(description = "Precio minimo del viaje", example = "2100")
         BigDecimal precioMinApp,
+
+        @Schema(description = "Precio maximo del viaje", example = "7800")
         BigDecimal precioMaxApp,
+
+        @Schema(description = "Hora que se pide el viaje", example "")
         LocalDateTime fechaHora
 ) {}
