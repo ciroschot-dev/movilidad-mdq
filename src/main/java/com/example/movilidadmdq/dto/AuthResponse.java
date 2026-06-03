e044a2ea4bab3547c894c705ef5278fd8f7845be
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 13:28:39 -0300

    swagger3

--- src/main/java/com/example/movilidadmdq/dto/AuthResponse.java
+++ src/main/java/com/example/movilidadmdq/dto/AuthResponse.java
@@ -2,4 +2,18 @@ package com.example.movilidadmdq.dto;
 
 import com.example.movilidadmdq.enums.Role;
 
-public record AuthResponse(Long id, String username, String email, String token, Role role) {}
+public record AuthResponse(
+        @Schema(descrption = "ID del usuario", example = "9999")
+        Long id,
+
+        @Schema(description = "Nombre de usuario", example = "morehidalggo")
+        String username,
+
+        @Schema(description = "Email del usuario", example = "morehidalggo@gmail.com")
+        String email,
+
+        @Schema(description = "Token de autorizacion", example = "TK872K0183")
+        String token,
+
+        @Schema(description = "Rol del usuario", example = "Admin")
+        Role role) {}
--- src/main/java/com/example/movilidadmdq/dto/TarifaRequest.java
+++ src/main/java/com/example/movilidadmdq/dto/TarifaRequest.java
@@ -6,6 +6,9 @@ import java.math.BigDecimal;
 
 @Data
 public class TarifaRequest {
+    @Schema(description= "precio base de taxi", example = "3000" )
     BigDecimal precioBase;
+
+    Schema(description="precio por km en taxi", example = "200")
     BigDecimal precioPorKm;
 }
