fb99a4c92634222eb30c0d53fd610b7b27651fb6
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 00:06:27 -0300

    swagger2

--- src/main/java/com/example/movilidadmdq/dto/CalculoViajeRequest.java
+++ src/main/java/com/example/movilidadmdq/dto/CalculoViajeRequest.java
@@ -1,13 +1,26 @@
 package com.example.movilidadmdq.dto;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record CalculoViajeRequest(
+        @Schema(description = "Nombre del origen", example = "Pinamar")
         @NotBlank String origen,
+
+        @Schema(description = "Nombre del origen", example = "Miramar")
         @NotBlank String destino,
+
+        @Schema(description = "Origen en coordenadas latitud", example = "-38.0")
         Double origenLat,
+
+        @Schema(description = "Origen en longitud", example = "-32.1200")
         Double origenLng,
+
+        @Schema(description = "Destino en coordenadas latitud", example = "-38.0")
         Double destinoLat,
+
+        @Schema(description = "Destino en longitud", example = "32.1200")
         Double destinoLng
 ) {
 }
--- src/main/java/com/example/movilidadmdq/dto/OpcionTransporteResponse.java
+++ src/main/java/com/example/movilidadmdq/dto/OpcionTransporteResponse.java
@@ -1,14 +1,25 @@
 package com.example.movilidadmdq.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import com.example.movilidadmdq.enums.TipoTransporte;
 
 import java.math.BigDecimal;
 
 public record OpcionTransporteResponse(
+
+        @Schema(description = "Tipo de transporte", example = "Uber")
         TipoTransporte tipo,
+
+        @Schema(description = "Precio minimo del viaje", example = "3400")
         BigDecimal precioMin,
+
+        @Schema(description = "Precio maximo del viaje", example = "6800")
         BigDecimal precioMax,
+
+        @Schema(description = "Tiempo en minutos de llegada", example = "6")
         int tiempoMinutos,
+
+        @Schema(description = "URL para redirigir a la opcion elegida", example = "www.uber.com/av%colon%1736")
         String url
 ) {
 }
