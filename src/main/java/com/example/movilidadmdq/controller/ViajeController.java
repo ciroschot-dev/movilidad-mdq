a5677c1428e1c1ca240fd4b9f2424dc8c16c7245
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-06-03 15:17:16 -0300

    SWAGGER

--- src/main/java/com/example/movilidadmdq/controller/ViajeController.java
+++ src/main/java/com/example/movilidadmdq/controller/ViajeController.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import java.util.List;
 
@@ -21,13 +24,11 @@ public class ViajeController
     private final UsuarioRepository usuarioRepository;
 
     @Operation(summary = "Calcular viaje", description = "Obtiene y calcula el viaje solicitado por el usuario")
-    @ApiResponse{
-        value =
-        @ApiResponse(responseCode ="200", description = "El viaje se ha calculado con exito")
-        @ApiResponse(responseCode = "400", description = "El calculo del viaje fallo")
-        @ApiResponse=(responseCode= "401", description = "Los datos ingresados son incorrectos")
-
-    }
+    @ApiResponses(value={
+        @ApiResponse(responseCode ="200", description = "El viaje se ha calculado con exito"),
+        @ApiResponse(responseCode = "400", description = "El calculo del viaje fallo"),
+        @ApiResponse(responseCode= "401", description = "Los datos ingresados son incorrectos")
+    })
 
     @PostMapping("/calcular")
     public ResponseEntity<List<OpcionTransporteResponse>> calcular(@Valid @RequestBody CalculoViajeRequest request, Authentication authentication)
